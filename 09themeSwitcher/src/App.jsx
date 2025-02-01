1fc09ee0830282f65cfe76afa408b902968bbb67
Subham <agarwalsubham0011@gmail.com>
2025-02-01 12:20:14 +0530

    Updated theme switcher feature

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './Contexts/theme'
 import Card from './components/Card'
@@ -26,13 +26,13 @@ function App() {
   return (
     <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
 
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+      <div className="w-[100vw] flex flex-wrap min-h-screen items-center">
+        <div className="w-[100vw]">
+          <div className="w-[100vw] max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">
+          <div className="w-[100vw] max-w-sm mx-auto">
             <Card />
 
           </div>
